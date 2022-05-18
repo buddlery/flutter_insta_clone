4fcc6996cf975bd423da6c724042058e7774cd97
Kipyo Yoo <kipryu@gmail.com>
2022-05-18 14:47:47 +0900

    init project

---
+++ android/app/src/main/java/kr/co/ibsoft/flutter_insta_clone/MainActivity.java
@@ -0,0 +1,6 @@
+package kr.co.ibsoft.flutter_insta_clone;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_insta_clone/src/app.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Insta Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(color: Colors.black),
+        ),
+      ),
+      home: const App(),
+    );
+  }
+}
